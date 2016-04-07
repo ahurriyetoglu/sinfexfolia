0638abb8fdc95c4ac26122c98207c5a522b8404b
Ali Hürriyetoğlu <ali.hurriyetoglu@gmail.com>
2016-04-07 10:55:15 +0200

    Create create_folia.py

---
+++ create_folia.py
@@ -0,0 +1,15 @@
+from pynlpl.formats import folia
+
+tweets_doc = folia.Document(id="tweetlist1")
+tweets_doc.declare(folia.Event, "https://raw.githubusercontent.com/ahurriyetoglu/sinfexfolia/master/sinfex.folia.event.xml")
+tweets_doc.declare(folia.Entity, "https://raw.githubusercontent.com/ahurriyetoglu/sinfexfolia/master/sinfex.foliaset.xml")
+
+textbody = tweets_doc.append(folia.Text)
+
+tweet = folia.Sentence(tweets_doc,generate_id_in=textbody)
+for t in tweets_df.text.values:
+    tweet.append(folia.Word, text=t) #,space=space)
+    #tweet.append(folia.Word, text="",space=space)
+    textbody.append(t)
+
+tweets_doc.save("tst.folia.xml")
