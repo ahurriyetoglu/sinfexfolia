358a4884e539254da3eae57b2fc314ecbae3e628
Ali Hürriyetoğlu <ali.hurriyetoglu@gmail.com>
2016-04-07 11:14:18 +0200

    Update create_folia.py

--- create_folia.py
+++ create_folia.py
@@ -1,13 +1,13 @@
 from pynlpl.formats import folia
 
 tweets_doc = folia.Document(id="tweetlist1")
-tweets_doc.declare(folia.Event, "https://raw.githubusercontent.com/ahurriyetoglu/sinfexfolia/master/sinfex.folia.event.xml")
-tweets_doc.declare(folia.Entity, "https://raw.githubusercontent.com/ahurriyetoglu/sinfexfolia/master/sinfex.foliaset.xml")
+tweets_doc.declare(folia.Event, "https://raw.githubusercontent.com/ahurriyetoglu/sinfexfolia/master/sinfex-event.foliaset.xml")
+tweets_doc.declare(folia.Entity, "https://raw.githubusercontent.com/ahurriyetoglu/sinfexfolia/master/sinfex-entities.foliaset.xml")
 
 textbody = tweets_doc.append(folia.Text)
 
 for tw in tweets_df.text.values[:250]:
-    tweet = folia.Event(tweets_doc, generate_id_in=textbody)
+    tweet = folia.Event(tweets_doc, cls='tweet',generate_id_in=textbody)
     for t in tw.split():
         tweet.append(folia.Word, text=t) #,space=space)
     #tweet.append(folia.Word, text="",space=space)
